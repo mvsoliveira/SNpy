29ef83ba662614bf01b5d4c736aa78bc1711d5a3
msilvaol <marcos.oliveira@cern.ch>
2019-01-31 14:04:39 +0100

    Added possibilitz of having internal registers for understanding the delaz in each of thre three internal blocks.

--- src/rtl/mh_compare_pt_and_find_max_pt_hierarchical.vhd
+++ src/rtl/mh_compare_pt_and_find_max_pt_hierarchical.vhd
@@ -7,8 +7,10 @@ use work.SortingHierarchical.all;
 
 entity mh_compare_pt_and_find_max_pt_hierarchical is
 	generic(num_in  : integer := CandidateNumber; -- Number of candidates to be sorted
-	        num_out : integer := 16);   -- Number of sorted muons
+	        num_out : integer := 16;    -- Number of sorted muons
+	        int_reg : boolean := False);
 	port(
+		clk       : in  std_logic;
 		muon_cand : in  MuonCandidateArray(0 to num_in - 1);
 		-- array with flags for the highest candidate
 		max_pt    : out std_logic_matrix(0 to num_out - 1)(0 to num_in - 1)
@@ -19,26 +21,57 @@ architecture RTL of mh_compare_pt_and_find_max_pt_hierarchical is
 
 	-- array of compare matrices, one per output candidate
 
-signal pt_compare_0 : pt_compare_matrix_t(0 to 0, 0 to num_in - 1)(0 to num_in - 2);
+	signal pt_compare_0      : pt_compare_matrix_t(0 to 0, 0 to num_in - 1)(0 to num_in - 2);
+	signal pt_compare_0_comb : pt_compare_matrix_t(0 to 0, 0 to num_in - 1)(0 to num_in - 2);
+
+	signal max_pt_comb : std_logic_matrix(0 to num_out - 1)(0 to num_in - 1);
 
 begin
 
-	mh_compare_pt_0_1: entity work.mh_compare_pt_0
-  generic map (
-    num_in  => num_in,
-    num_out => num_out)
-  port map (
-    muon_cand    => muon_cand,
-    pt_compare_0 => pt_compare_0);
-    
-    mh_compare_pt_1_and_find_max_1: entity work.mh_compare_pt_1_and_find_max
-  generic map (
-    num_in  => num_in,
-    num_out => num_out)
-  port map (
-    pt_compare_0 => pt_compare_0,
-    max_pt       => max_pt);
-    
-    
-    
+	mh_compare_pt_0_1 : entity work.mh_compare_pt_0
+		generic map(
+			num_in  => num_in,
+			num_out => num_out)
+		port map(
+			muon_cand    => muon_cand,
+			pt_compare_0 => pt_compare_0_comb);
+
+	int_reg_g : if int_reg generate
+	begin
+		process(clk) is
+		begin
+			if rising_edge(clk) then
+				pt_compare_0 <= pt_compare_0_comb;
+			end if;
+		end process;
+	end generate;
+
+	not_int_reg_g : if not int_reg generate
+	begin
+		pt_compare_0 <= pt_compare_0_comb;
+	end generate;
+
+	mh_compare_pt_1_and_find_max_1 : entity work.mh_compare_pt_1_and_find_max
+		generic map(
+			num_in  => num_in,
+			num_out => num_out)
+		port map(
+			pt_compare_0 => pt_compare_0,
+			max_pt       => max_pt_comb);
+
+	int_reg_g_2 : if int_reg generate
+	begin
+		process(clk) is
+		begin
+			if rising_edge(clk) then
+				max_pt <= max_pt_comb;
+			end if;
+		end process;
+	end generate;
+
+	not_int_reg_g_2 : if not int_reg generate
+	begin
+		max_pt <= max_pt_comb;
+	end generate;
+
 end architecture RTL;
--- src/rtl/muon_sorter_hierarchical.vhd
+++ src/rtl/muon_sorter_hierarchical.vhd
@@ -30,7 +30,8 @@ entity muon_sorter_hierarchical is
 
 	generic(num_in  : integer  := CandidateNumber; -- Number of candidates to be sorted
 	        num_out : integer  := 16;   -- Number of sorted muons
-	        delay   : positive := 1);   -- delay in clock cycles for pipeline register
+	        delay   : positive := 1;    -- delay in clock cycles for pipeline register
+	        int_reg : boolean  := False);
 
 	port(clk          : in  std_logic;
 	     sink_valid   : in  std_logic;
@@ -51,8 +52,13 @@ architecture behavior of muon_sorter_hierarchical is
 
 	-- pipeline
 	type sr_t is array (integer range <>) of MuonCandidateArray(0 to num_out - 1);
-	signal sr   : sr_t(0 to delay);
-	signal sr_v : std_logic_vector(0 to delay);
+	type muon_cand_int_reg_t is array (integer range <>) of MuonCandidateArray(0 to num_in - 1);
+	signal sr                 : sr_t(0 to delay);
+	signal sr_v               : std_logic_vector(0 to delay);
+	signal sink_valid_int_reg : std_logic_vector(0 to 2);
+	signal sink_valid_int     : std_logic;
+	signal muon_cand_int_reg  : muon_cand_int_reg_t(0 to 2);
+	signal muon_cand_int      : MuonCandidateArray(0 to num_in - 1);
 
 	attribute shreg_extract : string;
 	attribute shreg_extract of sr : signal is "no";
@@ -64,8 +70,10 @@ begin
 	mh_compare_pt_and_find_max_pt_hierarchical_0 : entity work.mh_compare_pt_and_find_max_pt_hierarchical
 		generic map(
 			num_in  => num_in,
-			num_out => num_out)
+			num_out => num_out,
+			int_reg => int_reg)
 		port map(
+			clk       => clk,
 			muon_cand => muon_cand,
 			max_pt    => max_pt);
 
@@ -76,14 +84,37 @@ begin
 			num_in  => num_in,
 			num_out => num_out)
 		port map(
-			muon_cand     => muon_cand,
+			muon_cand     => muon_cand_int,
 			max_pt        => max_pt,
 			top_cand_comb => top_cand_comb);
+
+	int_reg_g : if int_reg generate
+	begin
+		process(all) is
+		begin
+			sink_valid_int_reg(0) <= sink_valid;
+			muon_cand_int_reg(0)  <= muon_cand;
+			if rising_edge(clk) then
+				for i in 1 to 2 loop
+					sink_valid_int_reg(i) <= sink_valid_int_reg(i - 1);
+					muon_cand_int_reg(i)  <= muon_cand_int_reg(i - 1);
+				end loop;
+			end if;
+		end process;
+		sink_valid_int <= sink_valid_int_reg(2);
+		muon_cand_int  <= muon_cand_int_reg(2);
+	end generate;
+
+	not_int_reg_g : if not int_reg generate
+	begin
+		sink_valid_int <= sink_valid;
+		muon_cand_int  <= muon_cand;
+	end generate;
 
 	sr_p : process(all) is
 	begin
 		sr(0)   <= top_cand_comb;
-		sr_v(0) <= sink_valid;
+		sr_v(0) <= sink_valid_int;
 		if rising_edge(clk) then
 			for i in 1 to delay loop
 				sr(i)   <= sr(i - 1);
