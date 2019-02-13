63e95a00f80fd355b5a590b5d2b06eec29a5eb3c
Marcos Oliveira <marcos.oliveira@cern.ch>
2019-02-13 16:16:16 +0100

    Added shift register for muon sorter inputs to see if it changes I/O retiming bottlenecks.

--- src/tcl/generate_syn_runs_mux.tcl
+++ src/tcl/generate_syn_runs_mux.tcl
@@ -1,20 +1,18 @@
-proc create_run {I O D {run 1} opt} {
-    puts "Creating run with I = $I, O = $O, D = $D"
-    set prjpre [format "I%03d_O%03d_D%03d_IORET" $I $O $D]
+proc create_run {AW DW D {run 1} opt} {
+    puts "Creating run with AW = $AW, DW = $DW, D = $D"
+    set prjpre [format "AW%03d_DW%03d_D%03d" $AW $DW $D]
     set prjname [format "%s-%s" $prjpre $opt]
     set basepath "D:/mygitlab/sorting"
-    set prjpath [format "%s/syn/%s/wrapper_%s.prj" $basepath $prjname $prjname]
-    set subprjpath [format "%s/syn/%s/muon_sorter_1/muon_sorter_%s.prj" $basepath $prjname $prjname]
+    set prjpath [format "%s/syn/%s/wrapper_mux_%s.prj" $basepath $prjname $prjname]
+    set subprjpath [format "%s/syn/%s/mux_1/mux_%s.prj" $basepath $prjname $prjname]
     puts $prjpath
     puts $subprjpath
     project -new $prjpath
-    add_file -vhdl ${basepath}/src/rtl/MuctpiDataTypes.vhd
-    add_file -vhdl ${basepath}/src/rtl/MuctpiFunctions.vhd
-    add_file -verilog ${basepath}/src/rtl/lfsr.sv
-    add_file -vhdl ${basepath}/src/rtl/muon_sorter.vhd
+    add_file -vhdl ${basepath}/src/rtl/mux/mux.vhd
+    add_file -verilog ${basepath}/src/rtl/lfsr.sv    
     add_file -verilog ${basepath}/src/rtl/reducer.sv
-    add_file -vhdl ${basepath}/src/rtl/shift_reg_tap.vhd
-    add_file -vhdl ${basepath}/src/rtl/wrapper.vhd    
+    add_file -vhdl ${basepath}/src/rtl/shift_reg_tap.vhd    
+	add_file -vhdl ${basepath}/src/rtl/mux/wrapper_mux.vhd  
     set_option -disable_io_insertion 0
     set_option -part XCVU9P
     set_option -package FLGC2104
@@ -73,13 +71,13 @@ proc create_run {I O D {run 1} opt} {
 	    puts "Invalid opt option!"
 	}
     }
-    hdl_param -set I $I
-    hdl_param -set O $O
+    hdl_param -set AW $AW
+    hdl_param -set DW $DW
     hdl_param -set delay $D
     project -save $prjpath
     project -run compile
     cd ${basepath}/syn
-    export_project -instance muon_sorter_1 -add_file {../src/rtl/MuctpiDataTypes.vhd ../src/rtl/MuctpiFunctions.vhd ../src/rtl/muon_sorter.vhd} -no_default_hdl -project $subprjpath
+    export_project -instance mux_1 -add_file {../src/rtl/mux/mux.vhd} -no_default_hdl -project $subprjpath
 
     project_data -active $subprjpath
 
@@ -87,8 +85,8 @@ proc create_run {I O D {run 1} opt} {
     set_option -part XCVU9P
     set_option -package FLGC2104
     set_option -vhdl2008 1   
-    hdl_param -set num_in $I
-    hdl_param -set num_out $O
+    hdl_param -set AW $AW
+    hdl_param -set DW $DW
     hdl_param -set delay $D
     set_option -job pr_1 -add par
     set_option -job pr_1 -option enable_run 0
@@ -98,56 +96,56 @@ proc create_run {I O D {run 1} opt} {
 	    set_option -frequency 160
 	    set_option -fanout_limit 10000
 		add_file -constraint ${basepath}/src/xdc/clock_160.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq160retfan16 {
 	    set_option -retiming 1
 	    set_option -frequency 160
 	    set_option -fanout_limit 16
 		add_file -constraint ${basepath}/src/xdc/clock_160.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq80retfan10000 {
 	    set_option -retiming 1
 	    set_option -frequency 80
 	    set_option -fanout_limit 10000
 		add_file -constraint ${basepath}/src/xdc/clock_80.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq80retfan16 {
 	    set_option -retiming 1
 	    set_option -frequency 80
 	    set_option -fanout_limit 16
 		add_file -constraint ${basepath}/src/xdc/clock_80.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}	
 	freq320retfan10000 {
 	    set_option -retiming 1
 	    set_option -frequency 320
 	    set_option -fanout_limit 10000
 		add_file -constraint ${basepath}/src/xdc/clock_320.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq320retfan16 {
 	    set_option -retiming 1
 	    set_option -frequency 320
 	    set_option -fanout_limit 16
 		add_file -constraint ${basepath}/src/xdc/clock_320.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq40retfan10000 {
 	    set_option -retiming 1
 	    set_option -frequency 40
 	    set_option -fanout_limit 10000
 		add_file -constraint ${basepath}/src/xdc/clock_40.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}
 	freq40retfan16 {
 	    set_option -retiming 1
 	    set_option -frequency 40
 	    set_option -fanout_limit 16
 		add_file -constraint ${basepath}/src/xdc/clock_40.sdc
-		add_file -constraint ${basepath}/src/xdc/muon_sorter.sdc
+		add_file -constraint ${basepath}/src/xdc/mux.sdc
 	}	
 	default {
 	    puts "Invalid opt option!"
@@ -171,20 +169,14 @@ proc range {from to {step 1}} {
 
 set cfgs []
 set opts [list freq320retfan10000 freq320retfan16 freq160retfan10000 freq160retfan16 freq80retfan10000 freq80retfan16 freq40retfan10000 freq40retfan16]
-#lappend cfgs [list 16 16 [range 0 2]]
-#lappend cfgs [list 16 2 [range 0 2]]
-lappend cfgs [list 16 16 [range 0 3]]
-#lappend cfgs [list 32 16 [range 0 5]]
-#lappend cfgs [list 32 4 [range 0 5]]
-#lappend cfgs [list 48 16 [range 0 8]]
-#lappend cfgs [list 64 16 [range 0 10]]
+lappend cfgs [list 12 32 [range 0 3]]
 
 foreach cfg $cfgs {
-    foreach {I O Dr} $cfg {
+    foreach {AW DW Dr} $cfg {
 		foreach {D} $Dr {
 			foreach opt $opts {
-				puts "$I $O $D"
-				create_run $I $O $D 1 $opt
+				puts "$AW $DW $D"
+				create_run $AW $DW $D 1 $opt
 			}
 		}
     }
