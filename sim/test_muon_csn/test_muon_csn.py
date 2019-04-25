db6ced241330c2def02bfa93462385127b0e6349
msilvaol <marcos.oliveira@cern.ch>
2019-04-26 00:08:36 +0200

    Complete testbench for csn 352, 88, 64, and 88_64

--- sim/test_muon_csn/test_muon_csn.py
+++ sim/test_muon_csn/test_muon_csn.py
@@ -15,12 +15,13 @@ import copy
 
 class MyTB(object):
 
-    def __init__(self, dut, n, ratio):
+    def __init__(self, dut, n, ratio, toplevel):
         self.dut = dut
         self.n = n
+        self.toplevel = toplevel
         self.I = self.dut.I.value
         self.O = self.dut.O.value
-        self.delay = self.dut.delay.value
+        #self.delay = self.dut.delay.value
         self.val_cand_frac = ratio
         self.ptlen = self.dut.muon_i[0].pt.value.n_bits
         #self.sectorlen = self.dut.muon_i[0].sector.value.n_bits
@@ -36,12 +37,16 @@ class MyTB(object):
         self.dut._log.info('Simulation length: {len: d}'.format(len=self.n))
         self.dut._log.info('Number of candidates: input: {num_in:d} | output: {num_out:d}'.format(num_in=self.I, num_out=self.O))
         self.dut._log.info('Binary lengths: pt: {pt:d} | sector: {sector:d} | roi: {roi:d}'.format(pt=self.ptlen, sector=self.sectorlen, roi=self.roilen))
+        self.dut._log.info('Top-level: {tp:s}.'.format(tp=self.toplevel))
         #self.dut._log.info('Pre sort set {pre:s}'.format(pre=str(self.presort_in_sets)))
         self.dut._log.info('#' * 50)
 
         self.gen_muon()
         self.py_sort_muon()
-        self.py_net_sort_muon()
+        if self.toplevel == 'work.csn_sort_88_64':
+            self.py_net_88_64_sort_muon()
+        else:
+            self.py_net_sort_muon()
 
     
     @cocotb.coroutine
@@ -118,8 +123,32 @@ class MyTB(object):
         # copying list
         self.py_net_sorted_muon = copy.deepcopy(self.muon_cand)
         for i in range(self.n):
-            for j in self.list_of_pairs: self.SU.compare_and_swap(self.py_net_sorted_muon[i], *j, key='pt')
+            for j in self.list_of_pairs: self.SU.compare_and_swap(self.py_net_sorted_muon[i], *j, key=lambda k: k['pt'])
             self.py_net_sorted_muon[i] = self.py_net_sorted_muon[i][0:self.O]
+
+    def py_net_88_64_sort_muon(self):
+        # copying list
+        self.py_net_sorted_muon = copy.deepcopy(self.muon_cand)
+        self.net_sets_88 = self.SU.get_muctpi_opt_sets(88)
+        [self.list_of_pairs_88, net] = self.SU.get_muctpi_sel_net(gen_plots=False, net_sets=self.net_sets_88)
+        self.net_sets_64 = self.SU.get_muctpi_opt_sets(64)
+        [self.list_of_pairs_64, net] = self.SU.get_muctpi_sel_net(gen_plots=False, net_sets=self.net_sets_64)
+
+        factor = 4
+        for i in range(self.n):
+            data_a = []
+            data_b = []
+            for x in range(factor):
+                # sorting each of 4 parts
+                data_a.append(self.py_net_sorted_muon[i][x*self.I/factor:(x+1)*self.I/factor])
+                for j in self.list_of_pairs_88: self.SU.compare_and_swap(data_a[x], *j, key=lambda k: k['pt'])
+                # getting the highest from each subset
+                data_b.extend(data_a[x][0 : self.O])
+            # sorting the highest 64
+            for j in self.list_of_pairs_64: self.SU.compare_and_swap(data_b, *j, key=lambda k: k['pt'])
+            self.py_net_sorted_muon[i] = data_b[0:self.O]
+
+
 
 
 
@@ -144,15 +173,16 @@ def run_test(dut, ratio):
     Testing the Muon Sorter
     """
     n = int(os.environ['SIM_LEN'])
+    toplevel = os.environ['TOPLEVEL']
     #ratio = float(os.environ['VAL_CAND_FRAC'])
-    tb = MyTB(dut, n, ratio)
+    tb = MyTB(dut, n, ratio, toplevel)
     cocotb.fork(Clock(dut.clk, 10).start())
     stim_thread = cocotb.fork(tb.stim_muon())
     read_thread = cocotb.fork(tb.read_muon())
     yield stim_thread.join()
     yield read_thread.join()
     if tb.n <= 10:
-        tb.print_muon()
+       tb.print_muon()
     # checking only pt without using list  of pairs
     Pass = True
     for i in range(tb.n):
@@ -160,9 +190,9 @@ def run_test(dut, ratio):
             if tb.py_sorted_muon[i][j]['pt'] != tb.sim_sorted_muon[i][j]['pt']:
                 Pass = False
 
-    # checking all data using list of pairs comparison
+    #checking all data using list of pairs comparison
     if tb.py_net_sorted_muon != tb.sim_sorted_muon:
-        Pass = False
+       Pass = False
 
     if not Pass:
         raise TestFailure("Muon sorting failed, look the print report.")
@@ -175,4 +205,5 @@ def run_test(dut, ratio):
 # Generating Tests
 factory = TestFactory(run_test)
 factory.add_option("ratio", [r/10.0 for r in range(11)])
+#factory.add_option("ratio", [0.05])
 factory.generate_tests()
