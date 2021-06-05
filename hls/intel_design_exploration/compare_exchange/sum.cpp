af3d3e3dcb1df3021e5d840442a0ed51722a5300
Marcos Oliveira <marcos.oliveira@cern.ch>
2021-06-06 01:58:41 +0200

    Using struct for input and added consecutive inputs testing

--- hls/intel_design_exploration/compare_exchange/sum.cpp
+++ hls/intel_design_exploration/compare_exchange/sum.cpp
@@ -1,23 +1,34 @@
 #include "HLS/hls.h"
+#include "sum.h"
 #include<iostream>
 
 using namespace std;
 
 hls_stall_free_return
 //hls_always_run_component
-component int sum(int a, int b)
+component sumo_t sum(sumi_s_t i)
 {
 	// hls_fpga_reg ensures a output register is implemented
-    return hls_fpga_reg(a+b);
-    //return a+b;
+	sumo_t y = i.a+i.b+i.c+i.d;
+    //return hls_fpga_reg(y);
+    return y;
 }
 
 int main()
 {
-	int x,y,s;
-	x = 5;
-	y = 3;
-	s=sum(x,y);
-	cout<<"The sum is : "<<s << " \n";
-	return (s=8);
+	sumi_s_t x;
+	sumo_t y;
+	x.a = 5; x.b = 6; x.c = 7; x.d = 8;
+	y=sum(x);
+	x.a = 5; x.b = 6; x.c = 7; x.d = 9;
+	y=sum(x);
+	ihc_hls_enqueue(&y,&sum,x);
+	x.a = 9; x.b = 1; x.c = 2; x.d = 3;
+	ihc_hls_enqueue(&y,&sum,x);
+	x.a = 7; x.b = 6; x.c = 5; x.d = 4;
+	ihc_hls_enqueue(&y,&sum,x);
+	ihc_hls_component_run_all(&sum);
+//cout<<"The sum is : "<< y << " \n";
+	printf("The sum is : %d\n", y.to_int());
+	return (y.to_int()==22);
 }
--- hls/intel_design_exploration/compare_exchange/sum.h
+++ hls/intel_design_exploration/compare_exchange/sum.h
@@ -8,10 +8,25 @@
 #ifndef SUM_H_
 #define SUM_H_
 
+#include "HLS/ac_int.h"
 
 using namespace std;
 
-int sum(int, int);
+#define SUMI_WIDTH 28
+#define SUMO_WIDTH 30
+
+
+typedef ac_int<SUMI_WIDTH, false> sumi_t;
+typedef ac_int<SUMO_WIDTH, false> sumo_t;
+
+
+typedef struct {
+    sumi_t a;
+    sumi_t b;
+    sumi_t c;
+    sumi_t d;
+} sumi_s_t;
+
 
 
 #endif /* SUM_H_ */
