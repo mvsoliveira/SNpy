c20628b85058090b300401ea89fbe600fbed329e
Marcos Oliveira <marcos.oliveira@cern.ch>
2021-06-08 16:16:40 +0200

    using memory attribute

--- hls/intel_sources/sorting_nomux.h
+++ hls/intel_sources/sorting_nomux.h
@@ -22,28 +22,32 @@ typedef ac_int<ID_WIDTH, false> mid_t;
 typedef ac_int<ROI_WIDTH, false> mroi_t;
 typedef ac_int<FLG_WIDTH, false> mflg_t;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
     mid_t id;
     mpt_t pt;
     mroi_t roi;
     mflg_t flg;
 } oelement_t;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
     mpt_t pt;
     mroi_t roi;
     mflg_t flg;
 } ielement_t;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
 	oelement_t data[O];
 } oelement_s_t;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
+	oelement_t data[I];
+} ioelement_s_t;
+
+typedef struct __attribute__((packed)) {
 	ielement_t idata[I];
 } ielement_s_t;
 
-void compare_exchange(oelement_t data[I], const int a, const int b);
+ioelement_s_t compare_exchange(ioelement_s_t i, const int a, const int b);
 oelement_s_t compare_main(const ielement_s_t in);
 
 #endif
